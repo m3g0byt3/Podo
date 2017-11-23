243a005f3bdd3f888d7a60ff55b3ec39d08651a4
m3g0byt3 <m3g0byt3@gmail.com>
2017-11-23 17:02:06 +0300

    Added navbar + barbuttonitem

---
+++ Podo/Supporting Files/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Podo
+//
+//  Created by m3g0byt3 on 23/11/2017.
+//  Copyright © 2017 m3g0byt3. All rights reserved.
+//
+
+import Foundation
+
+struct AnimationDuration {
+    static let normal: TimeInterval = 0.30
+    static let short: TimeInterval = 0.15
+}
