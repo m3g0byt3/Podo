4b25008c81a01b640ec8550efc31d6638df9c928
m3g0byt3 <m3g0byt3@gmail.com>
2018-06-25 00:02:00 +0300

    Add `PaymentCardBackgroundView` custom UIView subclass

---
+++ Podo/Flows/TopUp Flow/View/Views/PaymentCardBackgroundView.swift
@@ -0,0 +1,11 @@
+//
+//  PaymentCardBackgroundView.swift
+//  Podo
+//
+//  Created by m3g0byt3 on 24/06/2018.
+//  Copyright © 2018 m3g0byt3. All rights reserved.
+//
+
+import UIKit
+
+final class PaymentCardBackgroundView: RoundShadowView, DoubleMargin {}
