57c27ea01509ab8ca0a72818f8aa93d4a9e675fd
m3g0byt3 <m3g0byt3@gmail.com>
2018-07-06 23:59:15 +0300

    POP: add `DoubleMarginView` custom UIView subclass

---
+++ Podo/Flows/TopUp Flow/View/Views/DoubleMarginView.swift
@@ -0,0 +1,11 @@
+//
+//  DoubleMarginView.swift
+//  Podo
+//
+//  Created by m3g0byt3 on 27/06/2018.
+//  Copyright © 2018 m3g0byt3. All rights reserved.
+//
+
+import UIKit
+
+final class DoubleMarginView: UIView, DoubleMargin {}
