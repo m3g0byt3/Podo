9a426de2e186b1f1db4f04e1746e29ee574f5eee
m3g0byt3 <m3g0byt3@gmail.com>
2018-07-18 18:27:47 +0300

    Initial version with separate events handler (`KeyboardHandler` instance)

---
+++ Podo/Extensions/KeyboardHandler.swift
@@ -0,0 +1,166 @@
+//
+//  KeyboardHandler.swift
+//  Podo
+//
+//  Created by m3g0byt3 on 11/07/2018.
+//  Copyright © 2018 m3g0byt3. All rights reserved.
+//
+
+import UIKit
+
+final class KeyboardHandler {
+
+    // MARK: - Constants
+
+    /// Notification names
+    private enum Names {
+
+        static var willShow: Notification.Name { return .UIKeyboardWillShow }
+        static var willHide: Notification.Name { return .UIKeyboardWillHide }
+    }
+
+    /// Various selectors
+    private enum Selectors {
+
+        static var shown: Selector { return #selector(keyboardShown(_:)) }
+        static var dismissed: Selector { return #selector(keyboardDismissed(_:)) }
+        static var done: Selector { return #selector(doneButtonHandler(_:)) }
+        static var back: Selector { return #selector(backButtonHandler(_:)) }
+        static var next: Selector { return #selector(nextButtonHandler(_:)) }
+        static var resign: Selector { return #selector(UIResponder.resignFirstResponder) }
+    }
+
+    // MARK: - Public Properties
+
+    weak var delegate: KeyboardHandling?
+
+    // MARK: - Private Properties
+
+    private lazy var customInputView: UIToolbar = { this in
+        let backButton = UIBarButtonItem(title: " ◀ ", style: .done, target: self, action: Selectors.back)
+        let nextButton = UIBarButtonItem(title: " ▶ ", style: .done, target: self, action: Selectors.next)
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: Selectors.done)
+        let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+
+        this.items = [backButton, nextButton, flexibleSpace, doneButton]
+        this.items?.forEach { $0.tintColor = .darkGray }
+        this.sizeToFit()
+
+        return this
+    }(UIToolbar())
+
+    private var scrollableViews: [UIScrollView] {
+        return delegate?.manageableViews.compactMap { $0 as? UIScrollView } ?? []
+    }
+
+    private var nonScrollableViews: [UIView] {
+        return delegate?.manageableViews.filter { !($0 is UIScrollView) } ?? []
+    }
+
+    private var offsetRatio: CGFloat {
+        return delegate?.keyboardOffsetRatio ?? 1.0
+    }
+
+    private var initialOffsets: [UIView: CGFloat]
+
+    // MARK: - Lifecycle
+
+    init(delegate: KeyboardHandling) {
+        self.delegate = delegate
+        self.initialOffsets = [:]
+        setupDelegate(delegate)
+        registerForNotifications(in: NotificationCenter.default)
+    }
+
+    deinit {
+        unregisterForNotifications(in: NotificationCenter.default)
+    }
+
+    // MARK: - Private API
+
+    private func setupDelegate(_ delegate: KeyboardHandling) {
+        delegate.manageableViews.forEach(setupInputAccessoryView(in:))
+    }
+
+    private func setupInputAccessoryView(in view: UIView) {
+        if let castedView = view as? _InputAccessoryViewProtocol {
+            castedView.inputAccessoryView = customInputView
+        } else {
+            view.subviews.forEach(setupInputAccessoryView)
+        }
+    }
+
+    private func registerForNotifications(in center: NotificationCenter) {
+        center.addObserver(self, selector: Selectors.shown, name: Names.willShow, object: nil)
+        center.addObserver(self, selector: Selectors.dismissed, name: Names.willHide, object: nil)
+    }
+
+    private func unregisterForNotifications(in center: NotificationCenter) {
+        center.removeObserver(self)
+    }
+
+    @objc private func keyboardShown(_ notification: Notification) {
+        guard let parsed = KeyboardNotification(notification) else { return }
+        let offset = parsed.offset * offsetRatio
+
+        // Handle not-scrollable views
+        guard let textInput = UIResponder.current as? UIView else { return }
+
+        for view in nonScrollableViews {
+            let textInputConvertedFrame = view.convert(textInput.frame, from: textInput)
+            let textInputMaxY = textInputConvertedFrame.maxY
+            let visibleHeight = view.frame.height - offset
+            let nonScrollableOffset = textInputMaxY - visibleHeight / 2
+
+            initialOffsets[view] = view.frame.origin.y
+
+            if textInputMaxY > visibleHeight {
+                UIView.animate(withDuration: parsed.duration, delay: 0, options: parsed.options, animations: {
+                    view.frame.origin.y -= min(offset, nonScrollableOffset)
+                })
+            }
+        }
+
+        // Handle scrollable views
+        guard parsed.endFrame != parsed.beginFrame else { return }
+
+        for view in scrollableViews {
+            view.contentInset.bottom += offset
+        }
+    }
+
+    @objc private func keyboardDismissed(_ notification: Notification) {
+        guard let parsed = KeyboardNotification(notification) else { return }
+        let offset = parsed.offset * offsetRatio
+
+        // Handle not-scrollable views
+        for view in nonScrollableViews {
+            UIView.animate(withDuration: parsed.duration, delay: 0, options: parsed.options, animations: {
+                view.frame.origin.y = self.initialOffsets[view, default: 0]
+            })
+        }
+
+        // Handle scrollable views
+        guard parsed.endFrame != parsed.beginFrame else { return }
+
+        for view in scrollableViews {
+            view.contentInset.bottom -= offset
+        }
+    }
+
+    private func centerForInput() {
+        // TODO: Calculate offset to place current first reponder in the center of visible rect
+    }
+
+    // MARK: - Button handlers
+
+    @objc private func doneButtonHandler(_ sender: UIBarButtonItem) {
+        UIApplication.shared.sendAction(Selectors.resign, to: nil, from: nil, for: nil)
+    }
+
+    @objc private func backButtonHandler(_ sender: UIBarButtonItem) {
+    }
+
+    @objc private func nextButtonHandler(_ sender: UIBarButtonItem) {
+    }
+}
--- Podo/Extensions/KeyboardHandling.swift
+++ Podo/Extensions/KeyboardHandling.swift
@@ -12,27 +12,21 @@ import ObjectiveC.runtime
 /// Constants for associated objects
 private enum AssociatedKeys {
 
-    static var tokensKey: Void?
-    static var initialOffset: Void?
-    static var inputView: Void?
-}
-
-/// Notification names
-private enum Names {
-
-    static var willShow: Notification.Name { return .UIKeyboardWillShow }
-    static var willHide: Notification.Name { return .UIKeyboardWillHide }
+    static var handler: Void?
 }
 
 /// Moving views behind keyboard automatically.
-protocol KeyboardHandling {
+protocol KeyboardHandling: class {
 
     /// (Required) Array of views that should be managed.
     var manageableViews: [UIView] { get }
 
-    /// (Optional) Ratio by which the keyboard height is multiplied. By default equals `1.0`.
+    /// (Optional) Ratio by which the keyboard height is multiplied.
+    /// By default equals `1.0`.
     var keyboardOffsetRatio: CGFloat { get }
 }
+
+// MARK: - Default implementation for optional protocol requirements
 
 extension KeyboardHandling {
 
@@ -44,130 +38,13 @@ extension KeyboardHandling {
 extension KeyboardHandling where Self: UIViewController {
 
     func beginKeyboardHandling() {
-        manageableViews.forEach(setupInputAccessoryView(in:))
-        beginObserving()
+        objc_setAssociatedObject(self,
+                                 &AssociatedKeys.handler,
+                                 KeyboardHandler(delegate: self),
+                                 .OBJC_ASSOCIATION_RETAIN)
     }
 
     func endKeyboardHandling() {
-        endObserving()
-    }
-}
-
-// MARK: - Keyboard handling
-
-private extension KeyboardHandling where Self: UIViewController {
-
-    var scrollableViews: [UIScrollView] {
-        return manageableViews.compactMap { $0 as? UIScrollView }
-    }
-
-    var nonScrollableViews: [UIView] {
-        return manageableViews.filter { !($0 is UIScrollView) }
-    }
-
-    var customInputView: UIToolbar {
-        get {
-            if let inputView = objc_getAssociatedObject(self, &AssociatedKeys.inputView) as? UIToolbar {
-                return inputView
-            }
-            let inputView = UIToolbar()
-            let backButton = UIBarButtonItem(title: "◀ ", style: .done, target: nil, action: nil)
-            let nextButton = UIBarButtonItem(title: " ▶", style: .done, target: nil, action: nil)
-            let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: nil)
-            let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
-
-            inputView.items = [backButton, nextButton, flexibleSpace, doneButton]
-            inputView.items?.forEach { $0.tintColor = .darkGray }
-            inputView.sizeToFit()
-            objc_setAssociatedObject(self, &AssociatedKeys.inputView, inputView, .OBJC_ASSOCIATION_RETAIN)
-
-            return inputView
-        }
-        set {
-            objc_setAssociatedObject(self, &AssociatedKeys.inputView, newValue, .OBJC_ASSOCIATION_RETAIN)
-        }
-    }
-
-    func keyboardShown(_ notification: Notification) {
-        guard let parsed = KeyboardNotification(notification) else { return }
-        let offset = parsed.offset * keyboardOffsetRatio
-
-        // Handle scrollable views
-        for view in scrollableViews where view.contentInset.bottom < offset {
-            view.contentInset.bottom += offset
-        }
-
-        // Handle not-scrollable views
-        guard let textInput = UIResponder.current as? UIView else { return }
-
-        for view in nonScrollableViews {
-            let textInputConvertedFrame = view.convert(textInput.frame, from: textInput)
-            let textInputMaxY = textInputConvertedFrame.maxY
-            let visibleHeight = view.frame.height - offset
-            let nonScrollableOffset = textInputMaxY - visibleHeight / 2
-
-            objc_setAssociatedObject(view, &AssociatedKeys.initialOffset, view.frame.origin.y, .OBJC_ASSOCIATION_ASSIGN)
-
-            if textInputMaxY > visibleHeight {
-                UIView.animate(withDuration: parsed.duration, delay: 0, options: parsed.options, animations: {
-                    view.frame.origin.y -= min(offset, nonScrollableOffset)
-                })
-            }
-        }
-    }
-
-    func keyboardDismissed(_ notification: Notification) {
-        guard let parsed = KeyboardNotification(notification) else { return }
-        let offset = parsed.offset * keyboardOffsetRatio
-
-        // Handle scrollable views
-        for view in scrollableViews where view.contentInset.bottom >= offset {
-            view.contentInset.bottom -= offset
-        }
-
-        // Handle not-scrollable views
-        let initialOffset = objc_getAssociatedObject(view, &AssociatedKeys.initialOffset) as? CGFloat ?? 0
-
-        for view in nonScrollableViews {
-            UIView.animate(withDuration: parsed.duration, delay: 0, options: parsed.options, animations: {
-                view.frame.origin.y = initialOffset
-            })
-        }
-    }
-
-    func setupInputAccessoryView(in view: UIView) {
-        if let castedView = view as? _InputAccessoryViewProtocol {
-            castedView.inputAccessoryView = customInputView
-        } else {
-            view.subviews.forEach(setupInputAccessoryView)
-        }
-    }
-    }
-}
-
-// MARK: - NotificationCenter stuff
-
-private extension KeyboardHandling where Self: UIViewController {
-
-    func beginObserving() {
-        let defaultNC = NotificationCenter.default
-        // TODO: observe notifications in separate `KeyboardHandler` class
-        let willShowToken = defaultNC.addObserver(forName: Names.willShow, object: nil, queue: nil) { [weak self] note in
-            self?.keyboardShown(note)
-        }
-        // TODO: observe notifications in separate `KeyboardHandler` class
-        let willHideToken = defaultNC.addObserver(forName: Names.willHide, object: nil, queue: nil) { [weak self] note in
-            self?.keyboardDismissed(note)
-        }
-        let tokens = [willShowToken, willHideToken]
-
-        objc_setAssociatedObject(self, &AssociatedKeys.tokensKey, tokens, .OBJC_ASSOCIATION_RETAIN)
-    }
-
-    func endObserving() {
-        if let tokens = objc_getAssociatedObject(self, &AssociatedKeys.tokensKey) as? [NSObjectProtocol] {
-            tokens.forEach(NotificationCenter.default.removeObserver(_:))
-        }
         objc_removeAssociatedObjects(self)
     }
 }
