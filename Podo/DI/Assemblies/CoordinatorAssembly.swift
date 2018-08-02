5697f5b91d1b693baea56763c1714c10017ac457
m3g0byt3 <m3g0byt3@gmail.com>
2018-08-03 00:56:25 +0300

    Resolve Router instead passing as parameter

--- Podo/DI/Assemblies/CoordinatorAssembly.swift
+++ Podo/DI/Assemblies/CoordinatorAssembly.swift
@@ -13,27 +13,51 @@ final class CoordinatorAssembly: Assembly {
 
     func assemble(container: Container) {
 
-        container.register(Coordinator.self) { _, router in
+        container.register(Coordinator.self) { resolver in
+            let dependencyType = RouterProtocol.self
+            guard let router = resolver.resolve(dependencyType) else {
+                unableToResolve(dependencyType)
+            }
             return ApplicationCoordinator(router: router, assembler: ApplicationAssembler.defaultAssembler)
         }
 
-        container.register(Coordinator.self, flow: .main) { _, router in
+        container.register(Coordinator.self, flow: .main) { resolver in
+            let dependencyType = RouterProtocol.self
+            guard let router = resolver.resolve(dependencyType) else {
+                unableToResolve(dependencyType)
+            }
             return MainMenuCoordinator(router: router, assembler: ApplicationAssembler.defaultAssembler)
         }
 
-        container.register(Coordinator.self, flow: .tutorial) { _, router in
+        container.register(Coordinator.self, flow: .tutorial) { resolver in
+            let dependencyType = RouterProtocol.self
+            guard let router = resolver.resolve(dependencyType) else {
+                unableToResolve(dependencyType)
+            }
             return TutorialCoordinator(router: router, assembler: ApplicationAssembler.defaultAssembler)
         }
 
-        container.register(SideMenuCoordinator.self, flow: .settings) { _, router in
+        container.register(SideMenuCoordinator.self, flow: .settings) { resolver in
+            let dependencyType = RouterProtocol.self
+            guard let router = resolver.resolve(dependencyType) else {
+                unableToResolve(dependencyType)
+            }
             return SettingsCoordinator(router: router, assembler: ApplicationAssembler.defaultAssembler)
         }
 
-        container.register(Coordinator.self, flow: .addNewCard) { _, router in
+        container.register(Coordinator.self, flow: .addNewCard) { resolver in
+            let dependencyType = RouterProtocol.self
+            guard let router = resolver.resolve(dependencyType) else {
+                unableToResolve(dependencyType)
+            }
             return AddNewCardCoordinator(router: router, assembler: ApplicationAssembler.defaultAssembler)
         }
 
-        container.register(Coordinator.self, flow: .topUp) { _, router in
+        container.register(Coordinator.self, flow: .topUp) { resolver in
+            let dependencyType = RouterProtocol.self
+            guard let router = resolver.resolve(dependencyType) else {
+                unableToResolve(dependencyType)
+            }
             return PaymentCoordinator(router: router, assembler: ApplicationAssembler.defaultAssembler)
         }
     }
