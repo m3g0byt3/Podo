98f139b255ac470bd5538711cde65c34e1c04b6e
m3g0byt3 <m3g0byt3@gmail.com>
2018-08-16 13:18:25 +0300

    Mark `sputnik` case as unavailable in TransportCardType model

--- Podo/Flows/Add New Card Flow/Model/TransportCardType.swift
+++ Podo/Flows/Add New Card Flow/Model/TransportCardType.swift
@@ -10,10 +10,40 @@ import Foundation
 
 /// Strongly-typed representation of trasport card type
 /// - Remark: Raw value = length of the card number for appropriate card type
-enum TransportCardType: Int {
+enum TransportCardType {
 
-    case sputnik = 11
-    case podorozhnikShort = 19
-    case podorozhnikLong = 26
-    case unknown = 0
+    private enum RawValues {
+        static let sputnik = 11
+        static let podorozhnikShort = 19
+        static let podorozhnikLong = 26
+        static let unknown = 0
+    }
+
+    @available(*, unavailable, message: "Not supported yet")
+    case sputnik
+    case podorozhnikShort
+    case podorozhnikLong
+    case unknown
+}
+
+// MARK: - RawRepresentable protocol conformance
+
+extension TransportCardType: RawRepresentable {
+
+    init?(rawValue: Int) {
+        switch rawValue {
+        case RawValues.podorozhnikShort: self = .podorozhnikShort
+        case RawValues.podorozhnikLong: self = .podorozhnikLong
+        case RawValues.unknown: self = .unknown
+        default: return nil
+        }
+    }
+
+    var rawValue: Int {
+        switch self {
+        case .podorozhnikShort: return RawValues.podorozhnikShort
+        case .podorozhnikLong: return RawValues.podorozhnikLong
+        case .unknown: return RawValues.unknown
+        }
+    }
 }
